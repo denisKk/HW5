110b95555f6a3f84cc229483936b9e89ae8f4fad
Denis Kulakov <kulakovdenis@mail.ru>
2023-12-01 18:42:43 +0300

    initial commit

---
+++ SuffixArray/SuffixArray/SuffixArrayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuffixArrayApp.swift
+//  SuffixArray
+//
+//  Created by Dev on 1.12.23.
+//
+
+import SwiftUI
+
+@main
+struct SuffixArrayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
